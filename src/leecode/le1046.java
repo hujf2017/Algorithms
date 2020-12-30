bc1ecbd88acbc1778c4b0386bd6a25e5a7a71ec9
胡剑峰2020 <952405831@qq.com>
2020-12-30 13:31:32 +0800

    算法贪心

---
+++ src/leecode/le1046.java
@@ -0,0 +1,25 @@
+package leecode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Hujf
+ * @title: le1046
+ * @date 2020/12/30 0030上午 9:54
+ * @description: TODO
+ */
+public class le1046 {
+    public int lastStoneWeight(int[] stones) {
+        Arrays.sort(stones);
+        int len = stones.length - 1;
+        for (int i = len; i > 0; i--) {
+            stones[len] = stones[len] - stones[len - 1];
+            stones[len - 1] = 0;
+            Arrays.sort(stones);
+        }
+        return stones[len];
+    }
+}
