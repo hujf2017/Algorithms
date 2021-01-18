cc4a666680f84b8675f75a7606f6cff2df58dd64
胡剑峰2020 <952405831@qq.com>
2021-01-18 20:30:57 +0800

    二分法的+1-1很精髓 注意

---
+++ src/leecode/le704.java
@@ -0,0 +1,32 @@
+package leecode;
+
+import org.junit.Test;
+
+/**
+ * @author Hujf
+ * @title: le704
+ * @date 2021/1/18 0018下午 8:22
+ * @description: TODO
+ */
+public class le704 {
+
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(target<nums[mid]){
+                end = mid-1;
+            }else if (target>nums[mid]){
+                start = mid+1;
+            }else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+    @Test
+    public void t(){
+        search(new int[]{-1,0,3,5,9,12},2);
+    }
+}
---
+++ src/leecode/le721.java
@@ -0,0 +1,21 @@
+package leecode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Hujf
+ * @title: le721
+ * @date 2021/1/18 0018下午 8:13
+ * @description: 太难了把
+ */
+public class le721 {
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        List<List<String>> lists = new ArrayList<>();
+        for(List a :accounts){
+
+        }
+        return lists;
+    }
+}
