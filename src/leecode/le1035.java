fdccbd3698127facae7670712e61738cbe713a90
hujf <952405831@qq.com>
2021-05-21 17:20:17 +0800

    动态规划最大相同 子序列

--- src/leecode/le1035.java
+++ src/leecode/le1035.java
@@ -11,19 +11,20 @@ package leecode;
 public class le1035 {
 
     public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        int len1 =0;
-        int len2 =0;
-
-        int count =0;
-        for(int i=0;i<nums1.length;i++){
-            if(nums1[len1]==nums2[len2]){
-                len1++;
-                len2++;
-                count++;
+        int len1 =nums1.length;
+        int len2 =nums2.length;
+        int [][] dp = new int[len1+1][len2+1];
+        for(int i=1;i<len1+1;i++){
+            int num = nums1[i-1];
+            for(int j=1;j<len2+1;j++){
+                int num2 =nums2[j-1];
+                if(num==num2){
+                    dp[i][j]= dp[i-1][j-1]+1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-
-        return count;
-
+        return dp[len1][len2];
     }
 }
